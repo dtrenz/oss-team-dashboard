7e677b983cdebeff78f53f3ecad62ad78a195055
Dan Trenz <dtrenz@gmail.com>
2016-03-30 17:47:59 -0400

    Slowing down kiosk rate

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@
 if (location.hash == "#kiosk") {
   $(function startKioskMode() {
 
-    var rate = 3000, // in ms
+    var rate = 20000, // in ms
         perPage = 6;
 
     var showCurrentPage = function() {
@@ -27,37 +27,3 @@ if (location.hash == "#kiosk") {
 
   });
 }
-
-// function startKioskMode() {
-//   var $items = $(".contributor");
-
-//   var rate = 3000; // in ms
-//   var currentPage = 0;
-//   var perPage = 6;
-//   var pageCount = Math.ceil(items.length / 6);
-
-//   var step = function() {
-//     var rangeStart = currentPage < pageCount ? currentPage * perPage : 0;
-//     var rangeEnd = rangeStart + perPage;
-
-//     console.info("step()", pageCount, perPage, currentPage, rangeStart, rangeEnd);
-
-//     for (var i = 0; i < items.length; i++) {
-//       var item = items[i];
-
-//       item.style.visibility = "visible";
-
-//       if (i >= rangeStart && i < rangeEnd) {
-//         item.hidden = false;
-//       } else {
-//         item.hidden = true;
-//       }
-//     }
-
-//     currentPage = currentPage < pageCount ? currentPage + 1 : 0;
-//   };
-
-//   step();
-
-//   setInterval(step2, rate);
-// }
